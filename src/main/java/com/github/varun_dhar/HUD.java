24dfc42125189d3744e8f6d9770f9a04c87ebc3d
doggo4242 <38188031+doggo4242@users.noreply.github.com>
2020-10-29 19:51:58 -0400

    Update HUD.java

--- src/main/java/com/github/varun_dhar/HUD.java
+++ src/main/java/com/github/varun_dhar/HUD.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 varun_dhar
+Copyright 2020 Varun Dhar
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
