b8b675ba7a851d6ddb888b70b58b2ec21f1f8b1d
unknown <awesomev@outlook.com>
2020-10-29 18:34:56 -0400

    Initial commit

---
+++ src/main/java/com/github/doggo4242/HUD5zig.java
@@ -0,0 +1,21 @@
+package com.github.doggo4242;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(HUD5zig.MODID)
+
+public class HUD5zig//setup stuff
+{
+	public static final String MODID = "hudmod";
+	public HUD5zig() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+	}
+	private void commonSetup(FMLCommonSetupEvent event)
+	{
+		MinecraftForge.EVENT_BUS.register(new RenderGuiHandler());
+	}
+
+}
---
+++ src/main/java/com/github/doggo4242/RenderGuiHandler.java
@@ -0,0 +1,16 @@
+package com.github.doggo4242;
+
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+/**
+ * Handles render events and sends them to overlay
+ */
+public class RenderGuiHandler
+{
+	@SubscribeEvent
+	public void onRenderGui(RenderGameOverlayEvent.Post event){
+		HUD hud = new HUD();
+		hud.overlay(event);
+	}
+}
