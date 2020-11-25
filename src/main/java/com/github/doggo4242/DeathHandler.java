9d197a863022d9dd8e73fe5a96bbab3e01bd1e01
doggo42 <awesomev@outlook.com>
2020-11-25 12:44:58 -0500

    Fixed death handler so it runs on servers with death screen disabled, and fixed my dumbass mistake where I reversed the condition registering the handler.

--- src/main/java/com/github/doggo4242/DeathHandler.java
+++ src/main/java/com/github/doggo4242/DeathHandler.java
@@ -1,15 +1,14 @@
 package com.github.doggo4242;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.DeathScreen;
-import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class DeathHandler
 {
 	@SubscribeEvent
-	public void onDeath(final GuiScreenEvent.DrawScreenEvent event){
-		if(event.getGui() instanceof DeathScreen && Minecraft.getInstance().player != null)
+	public void onDeath(final TickEvent.PlayerTickEvent event){
+		if(!event.player.isAlive() && Minecraft.getInstance().player != null)
 		{
 			System.out.println("uh oh ya deado");
 			HUD.dead = true;
