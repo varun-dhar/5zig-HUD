5d80a404f5177ebdc716e8dd527e31f2802e00c4
doggo42 <awesomev@outlook.com>
2020-12-09 22:28:38 -0500

    added macro parser and coordinate recorder

--- src/main/java/com/github/doggo4242/ConfigFile.java
+++ src/main/java/com/github/doggo4242/ConfigFile.java
@@ -2,7 +2,7 @@ package com.github.doggo4242;
 
 public class ConfigFile {
 	//probably a better way to do this, but i wanted it bundled
-	public static String contents = "#This is the config file for 5zigHUD.\n" +
+	public static String configContents = "#This is the config file for 5zigHUD.\n" +
 			"#Using this file, you can customize the features of the mod.\n" +
 			"#Lines prefaced with # signs are comments and are not read by 5zigHUD.\n" +
 			"#At this time, 5zigHUD lets you customize the position of the HUD and \n" +
@@ -27,4 +27,22 @@ public class ConfigFile {
 			"DeathTimerAlignment=-1\n" +
 			"DeathTimerEnabled=-1\n" +
 			"UpdaterEnabled=-1\n";
+	public static String macroContents = "#This is the config file for the macro feature of 5zig-HUD\n" +
+			"#Lines prefaced with # signs are comments and not read by 5zig-HUD\n" +
+			"#Macros are written in the format [macro name];[command/message]\n" +
+			"#For example, a macro that wrote the message \"Welcome\" when \"hi\" was inputted\n" +
+			"#would be written like this: hi;Welcome\n" +
+			"#Macros can also be added with commands\n" +
+			"#See https://github.com/doggo4242/5zig-HUD/wiki for more information\n";
+	public static String coordsContents = "#This is the config file for the coordinate mapping feature of 5zig-HUD\n" +
+			"#Lines prefaced with # signs are comments and are not read by 5zig-HUD\n" +
+			"#Coordinate mappings are written in the format \n" +
+			"#[location name];X: [x coordinate] Y: [y coordinate] Z: [z coordinate]\n" +
+			"#For example, a coordinate mapping that stored the \n" +
+			"#location \"home\" as X: 0 Y: 70 Z: 0 would be writen as \n" +
+			"#home;X: 0 Y: 70 Z: 0 \n" +
+			"#Mappings can be created with the saveCoords and saveCurrentCoords commands\n" +
+			"#Mappings can be printed by with the getCoords command, which takes the \n" +
+			"#location name as an argument\n" +
+			"#See https://github.com/doggo4242/5zig-HUD/wiki for more information\n";
 }
